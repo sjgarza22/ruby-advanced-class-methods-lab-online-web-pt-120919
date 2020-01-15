870f7607dc3ba523e9e78334e6cfce26ce9d59ac
Sara Garza <sjgarza22@gmail.com>
2020-01-15 00:59:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,46 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    song = self.new
+    song.save
+    
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{}
+  
+  end
 
   def self.all
     @@all
