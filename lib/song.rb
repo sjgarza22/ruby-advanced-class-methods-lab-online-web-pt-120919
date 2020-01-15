1e58e95800c94b439053a99a9281b11688f66528
Sara Garza <sjgarza22@gmail.com>
2020-01-15 01:26:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,14 @@ class Song
   
   end
   
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    
+    song
+  
+  end
+  
   def self.alphabetical
     self.all.sort_by{|song| song.name}
   
